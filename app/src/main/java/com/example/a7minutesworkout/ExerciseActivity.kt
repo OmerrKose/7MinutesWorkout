7b679905870079e0ec8d9acbb0a1f6f3ac533048
OmerrKose <omer.kose@ibtech.com.tr>
2021-05-21 12:56:58 +0300

    Added count down

--- app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -2,9 +2,17 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 
 
 class ExerciseActivity : AppCompatActivity() {
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise)
@@ -17,5 +25,45 @@ class ExerciseActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+        setRestProgressBar()
+    }
+
+    override fun onDestroy() {
+        if(restTimer != null) {
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+
+        super.onDestroy()
+    }
+
+    // Progress Bar Timer
+    private fun setRestProgressBar() {
+        findViewById<ProgressBar>(R.id.progressBar).progress = restProgress
+
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                findViewById<ProgressBar>(R.id.progressBar).progress = 10 - restProgress
+                findViewById<TextView>(R.id.progressBarTimer).text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "Exercise starts...",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }.start()
+    }
+
+    private fun setUpRestView() {
+        if(restTimer != null) {
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
     }
 }

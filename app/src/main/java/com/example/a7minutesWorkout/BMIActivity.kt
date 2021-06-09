6dc01cfc71c7f32eb540fd93dda48ded25d01a2e
OmerrKose <omer.kose@ibtech.com.tr>
2021-06-09 14:31:28 +0300

    Added imperial sytem to the BMI page

--- app/src/main/java/com/example/a7minutesWorkout/BMIActivity.kt
+++ app/src/main/java/com/example/a7minutesWorkout/BMIActivity.kt
@@ -5,16 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.Toolbar
 import com.example.a7minutesworkout.R
+import com.google.android.material.textfield.TextInputLayout
 import java.math.BigDecimal
 import java.math.RoundingMode
 
 class BMIActivity : AppCompatActivity() {
+
+    // Variables to check view, metric or imperial
+    private val metricUnitView = "METRIC_UNIT_VIEW"
+    private val imperialUnitView = "IMPERIAL_UNIT_VIEW"
+
+    private var currentVisibleView: String = metricUnitView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bmiactivity)
@@ -29,8 +35,8 @@ class BMIActivity : AppCompatActivity() {
 
         // Change the color of the status bar
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.statusBarColor = resources.getColor(R.color.colorPrimary);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = resources.getColor(R.color.colorPrimary)
         }
 
         // CALCULATE BUTTON
@@ -54,6 +60,48 @@ class BMIActivity : AppCompatActivity() {
             }
         }
 
+        makeMetricUnitsViewVisible()
+        findViewById<RadioGroup>(R.id.rgUnits).setOnCheckedChangeListener { _, checkedId ->
+            if(checkedId == R.id.rbMetricUnits)
+                makeMetricUnitsViewVisible()
+            else
+                makeImperialUnitsViewVisible()
+        }
+        
+
+    }
+
+    // Function to make Metric Units View available
+    private fun makeMetricUnitsViewVisible() {
+        currentVisibleView = metricUnitView
+        findViewById<TextInputLayout>(R.id.textInputMetricUnitWeight).visibility = View.VISIBLE
+        findViewById<TextInputLayout>(R.id.textInputMetricUnitHeight).visibility = View.VISIBLE
+
+        findViewById<TextInputLayout>(R.id.textInputImperialUnitsWeight).visibility = View.GONE
+        findViewById<LinearLayout>(R.id.linearLayoutImperialUnitsHeight).visibility = View.GONE
+
+        findViewById<LinearLayout>(R.id.displayBMIResults).visibility = View.GONE
+
+        // Clear edit texts
+        findViewById<AppCompatEditText>(R.id.editTextMetricUnitWeight).text!!.clear()
+        findViewById<AppCompatEditText>(R.id.editTextMetricUnitHeight).text!!.clear()
+    }
+
+    // Function to make Imperial Units View available
+    private fun makeImperialUnitsViewVisible() {
+        currentVisibleView = imperialUnitView
+        findViewById<TextInputLayout>(R.id.textInputMetricUnitWeight).visibility = View.GONE
+        findViewById<TextInputLayout>(R.id.textInputMetricUnitHeight).visibility = View.GONE
+
+        findViewById<TextInputLayout>(R.id.textInputImperialUnitsWeight).visibility = View.VISIBLE
+        findViewById<LinearLayout>(R.id.linearLayoutImperialUnitsHeight).visibility = View.VISIBLE
+
+        findViewById<LinearLayout>(R.id.displayBMIResults).visibility = View.GONE
+
+        // Clear edit texts
+        findViewById<AppCompatEditText>(R.id.editTextMetricUnitWeight).text!!.clear()
+        findViewById<AppCompatEditText>(R.id.editTextImperialUnitHeightFeet).text!!.clear()
+        findViewById<AppCompatEditText>(R.id.editTextImperialUnitHeightInch).text!!.clear()
     }
 
     // Function to compare the user inputs
@@ -87,15 +135,17 @@ class BMIActivity : AppCompatActivity() {
             bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
         }
 
-        val textViewYourBMI = findViewById<TextView>(R.id.textViewYourBMI)
+        // val textViewYourBMI = findViewById<TextView>(R.id.textViewYourBMI)
         val textViewBMIType = findViewById<TextView>(R.id.textViewBMIType)
         val textViewBMIValue = findViewById<TextView>(R.id.textViewBMIValue)
         val textViewBMIDescription = findViewById<TextView>(R.id.textViewBMIDescription)
 
-        textViewYourBMI.visibility = View.VISIBLE
+        findViewById<LinearLayout>(R.id.displayBMIResults).visibility = View.VISIBLE
+
+        /*textViewYourBMI.visibility = View.VISIBLE
         textViewBMIValue.visibility = View.VISIBLE
         textViewBMIType.visibility = View.VISIBLE
-        textViewBMIDescription.visibility = View.VISIBLE
+        textViewBMIDescription.visibility = View.VISIBLE*/
 
         BigDecimal(bmi.toDouble()).setScale(2, RoundingMode.HALF_EVEN).toString()
             .also { textViewBMIValue.text = it }
